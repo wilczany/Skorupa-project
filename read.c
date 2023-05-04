2e8c8eda588d9bce657434f2f1b458e332da8e12
wilczany <wilczewski.2407@gmail.com>
2023-05-04 05:21:56 +0200

    poprawka

--- read.c
+++ read.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define MAX_ARGUMENT_COUNT 32
+
 char* readLine(int *status){
     char *buf= malloc(sizeof(char)*512);
     int count= 0;
@@ -36,19 +38,19 @@ char* readLine(int *status){
     return buf;
 }
 
-char **separate (char *args, int *count){
-    char** result = malloc(sizeof(char*));
-    char* piece = strtok(args, " ");
+char **separate (char *args, int *count, char *delim){
+    char** result = malloc(sizeof(char*) * MAX_ARGUMENT_COUNT);
+    char* piece = strtok(args, delim);
     
     while(piece != NULL){
     
         result = realloc(result, ((*count)+1) * sizeof(char*));
         result[(*count)] = piece;
-        piece = strtok(NULL, " ");
+        piece = strtok(NULL, delim);
         (*count)++;
         
     }
-    result[(*count)]= 0;
+    result[(*count)]= NULL;
     return result;
     }
     
--- read.h
+++ read.h
@@ -3,7 +3,7 @@
 
 char *readLine(int *status);
 
-char **separate(char *args, int* count);
+char **separate(char *args, int* count, char *delim);
 
 char **seperatePipe(char *args,int *count);
 #endif
